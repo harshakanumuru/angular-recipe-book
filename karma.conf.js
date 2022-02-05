f99b68f2c2c3f2395fea8d76ff22a5e21abfc659
Harshavardhan <AG22146@ad.wellpoint.com>
2022-02-05 19:47:23 +0530

    Angular Recipe Book: Folder Restructure

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/ngapp'),
+      dir: require('path').join(__dirname, './coverage/angular-recipe-book'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/ngapp'));
+app.use(express.static(__dirname + '/dist/angular-recipe-book'));
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/ngapp/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/angular-recipe-book/index.html'));
 });
 app.listen(process.env.PORT || 8080);
